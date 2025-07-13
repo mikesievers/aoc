357d13ef8f9d6dd0f45a02bf8b7c8b4c23135784
Michael Sievers <michael@hamburg.de>
2025-07-13 13:47:40 +0000

    Make find_antennas a non-static method

--- src/app/day08/day08.py
+++ src/app/day08/day08.py
@@ -11,22 +11,21 @@ class Map:
             # Read all lines, but be sure to avoid worst cases like CR/LF on a line
             lines = [list(line.strip()) for line in f.readlines() if len(line) > 2]
             self._data = np.array(lines)
-            self._antennas = self.find_antennas(self._data)
+            self.find_antennas()
             self.find_antinodes()
 
-    @staticmethod
-    def find_antennas(data: np.ndarray) -> dict[str, tuple[int, int]]:
-        antennas = dict()
-        (max_y, max_x) = data.shape
+    def find_antennas(self):
+        self._antennas = dict()
+        assert isinstance(self._data, np.ndarray)
+        (max_y, max_x) = self._data.shape
         for y in range(max_y):
             for x in range(max_x):
-                curr_pos = data[y, x]
+                curr_pos = self._data[y, x]
                 if np.char.isalnum(curr_pos):
-                    if not antennas.get(curr_pos):
-                        antennas[curr_pos] = []
+                    if not self._antennas.get(curr_pos):
+                        self._antennas[curr_pos] = []
 
-                    antennas[curr_pos].append((int(y), int(x)))
-        return antennas
+                    self._antennas[curr_pos].append((int(y), int(x)))
 
     def find_antinodes(self):
         self._antinodes = []

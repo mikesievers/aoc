c55a8dc76f425417d49f151771cf848faecf1767
Michael Sievers <michael@hamburg.de>
2025-08-18 22:08:44 +0200

    Refactor file name day2

--- src/days/day02.rs
+++ src/days/day02.rs
@@ -1,6 +1,9 @@
 use std::fs::*;
 use std::io::{BufRead, BufReader};
 
+pub const INPUT: &str = "resources/day02_input.txt";
+pub const SAMPLE: &str = "resources/day02_sample.txt";
+
 pub fn read_input(fname: &str) -> Vec<String> {
     let file = File::open(fname).unwrap();
     let reader = BufReader::new(file);
--- tests/day02.rs
+++ tests/day02.rs
@@ -1,7 +1,8 @@
 use aoc::days::*;
 use rstest::rstest;
 
+
 #[test]
 fn test_read_input(){
-    let lines = day02::read_input("resources/day02_input.txt");
+    let lines = day02::read_input(day02::INPUT);
 }

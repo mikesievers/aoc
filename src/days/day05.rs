0bbf952c466cc417ba55c323542e467795c2d3f5
Michael Sievers <michael@hamburg.de>
2025-08-24 18:48:01 +0200

    Begin day 05

---
+++ src/days/day05.rs
@@ -0,0 +1,14 @@
+// The data to parse for day 05 will be used
+// to explore the nom crate for data parsing.
+
+struct Almanac {}
+
+impl Almanac {
+    fn from_file(fname: &str) -> Self {
+        Self {}
+    }
+}
+#[test]
+fn test_parse_input() {
+    let almanac = Almanac::from_file("resources/day05_input.txt");
+}

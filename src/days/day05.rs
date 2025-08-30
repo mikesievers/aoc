585647b800b605a5d5a228286d07c5f9b49011b4
Michael Sievers <michael@hamburg.de>
2025-08-30 23:15:39 +0200

    Parse complete sample file

--- src/days/day05.rs
+++ src/days/day05.rs
@@ -1,5 +1,7 @@
 // The data to parse for day 05 will be used
+
 // to explore the nom crate for data parsing.
+
 // See https://docs.rs/nom/8.0.0/nom/index.html
 
 use nom::{
@@ -9,7 +11,7 @@ use nom::{
     combinator::{map, recognize},
     error::Error,
     multi::{many0, many1, separated_list1},
-    sequence::{delimited, preceded, terminated},
+    sequence::{delimited, preceded, terminated, tuple},
 };
 
 use std::fs::{self, read_to_string};
@@ -17,47 +19,96 @@ use std::fs::{self, read_to_string};
 pub struct Almanac {
     pub seeds: Vec<i32>,
     pub seed_to_soil: Vec<Vec<i32>>,
+    pub soil_to_fertilizer: Vec<Vec<i32>>,
+    pub fertilizer_to_water: Vec<Vec<i32>>,
+    pub water_to_light: Vec<Vec<i32>>,
+    pub light_to_temperature: Vec<Vec<i32>>,
+    pub temperature_to_humidity: Vec<Vec<i32>>,
+    pub humidity_to_location: Vec<Vec<i32>>,
 }
 
 impl Almanac {
     fn from_file(fname: &str) -> Self {
         let input = fs::read_to_string(fname).expect("Could not read input file.");
 
-        // Parser for a single integer
-
-        //let seeds = vec![];
-        let seed_to_soil = vec![];
-        let (input, seeds) = seed_section.parse(input.as_str()).unwrap();
+        let (
+            input,
+            (
+                seeds,
+                seed_to_soil,
+                soil_to_fertilizer,
+                fertilizer_to_water,
+                water_to_light,
+                light_to_temperature,
+                temperature_to_humidity,
+                humidity_to_location,
+            ),
+        ) = (
+            seed_section,
+            parse_named_section("seed-to-soil"),
+            parse_named_section("soil-to-fertilizer"),
+            parse_named_section("fertilizer-to-water"),
+            parse_named_section("water-to-light"),
+            parse_named_section("light-to-temperature"),
+            parse_named_section("temperature-to-humidity"),
+            parse_named_section("humidity-to-location"),
+        )
+            .parse(input.as_str())
+            .unwrap();
 
         Self {
             seeds,
             seed_to_soil,
+            soil_to_fertilizer,
+            fertilizer_to_water,
+            water_to_light,
+            light_to_temperature,
+            temperature_to_humidity,
+            humidity_to_location,
         }
     }
 }
 
 // Parser
+
 // Parse a file like:
+
 //
+
 // seeds: 79 14 55 13
 
 // seed-to-soil map:
+
 // 50 98 2
+
 // 52 50 48
 
 // soil-to-fertilizer map:
+
 // 0 15 37
+
 // 37 52 2
+
 // 39 0 15
 
 #[test]
+
 fn test_parse_input() {
     let almanac = Almanac::from_file("resources/day05_sample.txt");
 
     assert_eq!(almanac.seeds, [79, 14, 55, 13]);
+
+    assert_eq!(almanac.seed_to_soil, [[50, 98, 2], [52, 50, 48]]);
+
+    assert_eq!(
+        almanac.soil_to_fertilizer,
+        [[0, 15, 37], [37, 52, 2], [39, 0, 15]]
+    );
+    assert_eq!(almanac.humidity_to_location, [[60, 56, 37], [56, 93, 4]]);
 }
 
 // nom experimentation area
+
 fn decimal_line(input: &str) -> IResult<&str, Vec<i32>> {
     terminated(separated_list1(tag(" "), i32), line_ending).parse(input)
 }
@@ -66,37 +117,52 @@ fn seed_section(input: &str) -> IResult<&str, Vec<i32>> {
     preceded(tag("seeds: "), decimal_line).parse(input)
 }
 
-// TODO: expand this copy of seed_section to the text section
-// then generalize it to a named section via a higher order function
-// that returns a parser for that specific section
-fn parse_section(input: &str) -> IResult<&str, Vec<Vec<i32>>> {
-    preceded((tag("\r\nseed-to-soil map:"),line_ending), many1(decimal_line)).parse(input)
+// Parse one section like
+
+// seed-to-soil map:
+
+// 50 98 2
+
+// 52 50 48
+
+fn parse_named_section(section_name: &str) -> impl Fn(&str) -> IResult<&str, Vec<Vec<i32>>> {
+    let header = format!("\r\n{} map:", section_name);
+
+    move |input: &str| {
+        preceded((tag(header.as_str()), line_ending), many1(decimal_line)).parse(input)
+    }
 }
 
-
-
 #[test]
+
 fn test_parsing() {
     let mystr = "10000";
+
     //let res = mystr.parse::<i32>().unwrap();
 
     //let res = decimal_nr.parse(mystr).unwrap();
+
     //let res: i32 = nom::character::complete::i32::<&str, nom::error::Error<&str>>(mystr).unwrap().1;
+
     let res: i32 = i32::<&str, Error<&str>>(mystr).unwrap().1;
 
     assert_eq!(res, 10000);
 
     let mystr = "1 2 3\r\n";
+
     let res = decimal_line.parse(mystr).unwrap();
 
     assert_eq!(res.1, vec![1, 2, 3]);
+
     assert_eq!(res.0, "");
 
     let input = read_to_string("resources/day05_sample.txt").unwrap();
+
     let seeds_res = seed_section.parse(input.as_str()).unwrap();
 
     assert_eq!(seeds_res.1, vec![79, 14, 55, 13]);
 
-    let seed_to_soil_res = parse_section(seeds_res.0).unwrap();
-    assert_eq!(seed_to_soil_res.1, vec![vec![50,98, 2], vec![52,50,48]]);
+    let seed_to_soil_res = parse_named_section("seed-to-soil")(seeds_res.0).unwrap();
+
+    assert_eq!(seed_to_soil_res.1, vec![vec![50, 98, 2], vec![52, 50, 48]]);
 }

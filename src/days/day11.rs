9a7ae361d149c738e5b30b721dc8ecaae937e89d
Michael Sievers <michael@hamburg.de>
2025-09-13 09:50:34 +0200

    Calculate expanded universe

--- src/days/day11.rs
+++ src/days/day11.rs
@@ -2,6 +2,7 @@ use std::{collections::HashSet, fs::read_to_string};
 
 struct Universe {
     space: Vec<Vec<char>>,
+    galaxies: Vec<(usize, usize)>,
     empty_rows: HashSet<usize>,
     empty_columns: HashSet<usize>,
 }
@@ -14,32 +15,57 @@ impl Universe {
             .into_iter()
             .map(|line| line.chars().collect())
             .collect();
-        let (empty_rows, empty_columns) = Universe::empty_rows_columns(&space);
+        let (empty_rows, empty_columns, galaxies) = Universe::parse_universe(&space);
 
         Universe {
             space,
+            galaxies,
             empty_rows,
             empty_columns,
         }
     }
 
-    pub fn empty_rows_columns(space: &Vec<Vec<char>>) -> (HashSet<usize>, HashSet<usize>) {
+    fn parse_universe(
+        space: &Vec<Vec<char>>,
+    ) -> (HashSet<usize>, HashSet<usize>, Vec<(usize, usize)>) {
         // First, assume space is empty
-        
+
         let mut empty_rows = HashSet::from_iter((0..space.len()).into_iter());
         let mut empty_columns = HashSet::from_iter(0..space.len());
+        let mut galaxies = Vec::new();
         for (y, row) in space.iter().enumerate() {
             for (x, cell) in row.iter().enumerate() {
                 match cell {
                     '#' => {
-                        empty_rows.retain(|&row_idx| row_idx != y);
-                        empty_columns.retain(|&col_idx| col_idx != x);
+                        empty_rows.remove(&y);
+                        empty_columns.remove(&x);
+                        galaxies.push((y, x));
                     }
                     _ => {}
                 }
             }
         }
-        (empty_rows, empty_columns)
+        (empty_rows, empty_columns, galaxies)
+    }
+
+    fn expanded_galaxies(&self) -> Vec<(usize, usize)> {
+        let mut expanded_galaxies = Vec::new();
+
+        for galaxy in &self.galaxies {
+            let space_before_y = self
+                .empty_rows
+                .iter()
+                .filter(|&&row_idx| row_idx < galaxy.0)
+                .count();
+            let space_before_x = self
+                .empty_columns
+                .iter()
+                .filter(|&&col_idx| col_idx < galaxy.1)
+                .count();
+            expanded_galaxies.push((galaxy.0 + space_before_y, galaxy.1 + space_before_x));
+        }
+
+        expanded_galaxies
     }
 }
 
@@ -51,4 +77,22 @@ pub fn test_universe() {
     assert_eq!(*universe.space.get(1).unwrap().get(1).unwrap(), '.');
     assert_eq!(universe.empty_rows, HashSet::from_iter(vec![3, 7]));
     assert_eq!(universe.empty_columns, HashSet::from_iter(vec![2, 5, 8]));
+    assert_eq!(
+        universe.galaxies,
+        vec![
+            (0, 3),
+            (1, 7),
+            (2, 0),
+            (4, 6),
+            (5, 1),
+            (6, 9),
+            (8, 7),
+            (9, 0),
+            (9, 4)
+        ]
+    );
+    assert_eq!(
+        universe.expanded_galaxies(),
+        vec![(0, 4), (1, 9), (2, 0), (5, 8), (6, 1), (7, 12), (10, 9), (11, 0), (11, 5)]
+    );
 }

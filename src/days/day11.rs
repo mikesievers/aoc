e4acc4c61a82dc0bf182a820c10c7fba6f7a896c
Michael Sievers <michael@hamburg.de>
2025-09-13 09:34:19 +0200

    Identify empty columns, rows

--- src/days/day11.rs
+++ src/days/day11.rs
@@ -1,7 +1,9 @@
-use std::fs::read_to_string;
+use std::{collections::HashSet, fs::read_to_string};
 
 struct Universe {
     space: Vec<Vec<char>>,
+    empty_rows: HashSet<usize>,
+    empty_columns: HashSet<usize>,
 }
 
 impl Universe {
@@ -12,8 +14,32 @@ impl Universe {
             .into_iter()
             .map(|line| line.chars().collect())
             .collect();
+        let (empty_rows, empty_columns) = Universe::empty_rows_columns(&space);
 
-        Universe { space }
+        Universe {
+            space,
+            empty_rows,
+            empty_columns,
+        }
+    }
+
+    pub fn empty_rows_columns(space: &Vec<Vec<char>>) -> (HashSet<usize>, HashSet<usize>) {
+        // First, assume space is empty
+        
+        let mut empty_rows = HashSet::from_iter((0..space.len()).into_iter());
+        let mut empty_columns = HashSet::from_iter(0..space.len());
+        for (y, row) in space.iter().enumerate() {
+            for (x, cell) in row.iter().enumerate() {
+                match cell {
+                    '#' => {
+                        empty_rows.retain(|&row_idx| row_idx != y);
+                        empty_columns.retain(|&col_idx| col_idx != x);
+                    }
+                    _ => {}
+                }
+            }
+        }
+        (empty_rows, empty_columns)
     }
 }
 
@@ -23,4 +49,6 @@ pub fn test_universe() {
     assert_eq!(universe.space.len(), 10);
     assert_eq!(*universe.space.get(2).unwrap().get(0).unwrap(), '#');
     assert_eq!(*universe.space.get(1).unwrap().get(1).unwrap(), '.');
+    assert_eq!(universe.empty_rows, HashSet::from_iter(vec![3, 7]));
+    assert_eq!(universe.empty_columns, HashSet::from_iter(vec![2, 5, 8]));
 }

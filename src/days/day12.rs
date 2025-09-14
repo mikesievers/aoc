3406522da4c2bad9a441e3f47443ad146fae161c
Michael Sievers <michael@hamburg.de>
2025-09-14 15:45:14 +0200

    Handle "must match" situations

--- src/days/day12.rs
+++ src/days/day12.rs
@@ -64,35 +64,6 @@ fn test_records() {
     assert_eq!(ledger.sum_match_counts(), 21);
 }
 
-fn count_matches(line: &str, groups: &Vec<usize>) -> usize {
-    let mut count = 0;
-    let mut chars: Vec<char> = line.chars().collect();
-    let jokers: Vec<usize> = chars
-        .iter()
-        .enumerate()
-        .filter(|(_, c)| **c == '?')
-        .map(|(idx, _)| idx)
-        .collect();
-
-    // loop over all possible combinations
-
-    let states = ['.', '#'];
-    let combinations = std::iter::repeat(states.iter().cloned())
-        .take(jokers.len())
-        .multi_cartesian_product();
-
-    for combination in combinations {
-        for (idx, state) in combination.iter().enumerate() {
-            chars[jokers[idx]] = *state;
-        }
-        let groups_found = get_groups(&chars);
-        if groups_found == *groups {
-            count += 1;
-        }
-    }
-    count
-}
-
 fn get_groups(chars: &Vec<char>) -> Vec<usize> {
     let mut groups = Vec::new();
 
@@ -121,20 +92,56 @@ fn test_get_groups() {
     assert_eq!(grps, Vec::<usize>::from([2, 1, 3, 1]));
 }
 
+fn count_matches(line: &str, groups: &Vec<usize>) -> usize {
+    // When all groups have been consume, count as a match
+    // And allow recursion to wrap up
+    if groups.len() == 0 {
+        return 1;
+    }
+
+    // If the remaining string is smaller than the group size, it can't be a match
+    let grp_size = groups[0];
+    if groups.len() < grp_size {
+        return 0;
+    }
+
+    let mut count = 0;
+
+    for skips in 0..=(line.len() - grp_size) {
+        let sub_line = line.chars().skip(skips).collect::<String>();
+        if is_group_matching(&sub_line, grp_size).unwrap() {
+            count += 1;
+        }
+    }
+
+    count
+}
+
 #[test]
 fn test_count_matches() {
     let str1 = "???.###";
+    assert_eq!(count_matches(str1, &Vec::from([1])), 3);
     assert_eq!(count_matches(str1, &Vec::from([1, 3])), 3);
+
+    let str2 = "";
+    assert_eq!(count_matches(str2, &Vec::from([1])), 0);
+
+    let str3 = "???.##";
+    assert_eq!(count_matches(str3, &Vec::from([1, 3])), 0);
 }
 
-fn is_group_matching(input: &String, grp_size: usize) -> bool {
+// Check whether the string at the current position matches the current group
+fn is_group_matching(input: &String, grp_size: usize) -> Option<bool> {
     // A string that is too short can't match
     if input.len() < grp_size {
-        return false;
+        return Some(false);
     };
 
     // this is what a matching default group would look like
     let group_string = "#".repeat(grp_size);
+
+    // If any spring it marked as defect, it must be accounted for by a match
+    let must_match = input.chars().take(grp_size).any(|c| c == '#');
 
     // prepare a substring of the first grp_size characters where it is assumed
     // that '?' means defect spring (otherwise it would not match)
@@ -147,24 +154,44 @@ fn is_group_matching(input: &String, grp_size: usize) -> bool {
     // If the string with assumed defect does not match the group string, it's
     // not a match
     if assume_defect != group_string {
-        return false;
-    }
+        match must_match {
+            true => return None, // A match was mandatory, this means the string can't match
+            false => return Some(false), // The string just does not match here, but that's OK
+        }
+    };
 
     // if the following character is also a '#', it's not a match
     // (the group would have needed to be longer)
+    // Also, since all defects must be matched, this can't be counted
     if input.len() > grp_size && input.chars().nth(grp_size) == Some('#') {
-        return false;
+        return None;
     } else {
         // This was the end of the string or the following char is . or ?
-        return true;
+        return Some(true);
     }
 }
 
 #[test]
 fn test_is_group_matching() {
-    assert_eq!(is_group_matching(&".#.".to_string(), 2), false);
-    assert_eq!(is_group_matching(&"?#.".to_string(), 2), true);
-    assert_eq!(is_group_matching(&"##.".to_string(), 2), true);
-    assert_eq!(is_group_matching(&"###".to_string(), 2), false);
-    assert_eq!(is_group_matching(&"##".to_string(), 2), true);
+    assert_eq!(is_group_matching(&"".to_string(), 2), Some(false));
+    assert_eq!(is_group_matching(&".#.".to_string(), 2), None);
+    assert_eq!(is_group_matching(&"?#.".to_string(), 2), Some(true));
+    assert_eq!(is_group_matching(&"##.".to_string(), 2), Some(true));
+    assert_eq!(is_group_matching(&"##.".to_string(), 1), None);
+    assert_eq!(is_group_matching(&"###".to_string(), 2), None);
+    assert_eq!(is_group_matching(&"##".to_string(), 2), Some(true));
+}
+
+fn get_rest(input: &String, grp_size: usize) -> Option<String> {
+    let rest = input.chars().skip(grp_size + 1).collect::<String>();
+    if rest.len() > 0 { Some(rest) } else { None }
+}
+
+#[test]
+fn test_get_rest() {
+    assert_eq!(get_rest(&".#.12".to_string(), 2), Some("12".to_string()));
+    assert_eq!(get_rest(&".#?#12".to_string(), 3), Some("12".to_string()));
+    assert_eq!(get_rest(&".#?".to_string(), 3), None);
+    assert_eq!(get_rest(&".#?".to_string(), 2), None);
+    assert_eq!(get_rest(&"".to_string(), 2), None);
 }

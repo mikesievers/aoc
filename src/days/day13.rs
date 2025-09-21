31f8eb6051881da814344ebe216fee9fb0543085
Michael Sievers <michael@hamburg.de>
2025-09-21 11:12:13 +0200

    Parse one map line

--- src/days/day13.rs
+++ src/days/day13.rs
@@ -1,4 +1,38 @@
+use std::fs::read_to_string;
+
+use nom::{character::complete::{newline, one_of}, multi::many1, sequence::terminated, IResult, Parser};
+
+struct Map {
+    data: Vec<Vec<char>>,
+}
+
+struct Atlas {
+    maps: Vec<Map>,
+}
+
+impl Atlas {
+    pub fn from_file(fname: &str) -> Self {
+        let input =
+            read_to_string(fname).unwrap_or_else(|_| panic!("Could not read input file {fname}"));
+
+        let maps = Vec::new();
+
+        Atlas { maps }
+    }
+}
+
 #[test]
-fn test_something(){
-    ()
+fn test_atlas() {
+    let atlas = Atlas::from_file("resources/day13_sample.txt");
+
+    assert_eq!(atlas.maps.len(), 2);
+}
+
+fn map_line(input: &str) -> IResult<&str, Vec<char>> {
+    terminated(many1(one_of(".#")), newline).parse(input)
+}
+
+#[test]
+fn test_map_line() {
+    assert_eq!(map_line.parse(".#.\n").unwrap(), ("", vec!['.', '#', '.']));
 }

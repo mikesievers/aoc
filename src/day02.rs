4e6d5a68d87b253b5eddec13bb04ddc6be854248
Michael Sievers <michael@hamburg.de>
2025-12-14 21:57:26 +0100

    Solved day02 part 1

--- src/day02.rs
+++ src/day02.rs
@@ -1,4 +1,10 @@
 //! Advent of Code day02
+//! ```
+//! use  aoc::day02::{IDRanges, sum_invalid_ids};
+//!
+//! let idranges = IDRanges::from_file("input/day02_input.txt");
+//! assert_eq!(sum_invalid_ids(&idranges), 19605500130);
+//! ```
 use std::fs::read_to_string;
 
 use nom::{
@@ -10,7 +16,7 @@ use nom::{
 };
 
 #[derive(PartialEq, Debug)]
-struct IDRange {
+pub struct IDRange {
     start: u64,
     end: u64,
 }
@@ -19,7 +25,7 @@ pub struct IDRanges {
     // The ranges have the following features:
     // 1. start and end can have different lengths
     // 2. end length is at most one more than the start
-    ranges: Vec<IDRange>,
+    pub ranges: Vec<IDRange>,
 }
 
 impl IDRanges {
@@ -48,11 +54,46 @@ fn parse_range_file(input: &str) -> IResult<&str, Vec<IDRange>> {
     Ok((rest, ranges))
 }
 
+// id considerations
+fn is_invalid_id(id: u64) -> bool {
+    let id_string = id.to_string();
+    let id_len = id_string.len();
+
+    if (id_len % 2) != 0 {
+        return false;
+    }
+
+    if id_string[0..id_len / 2] == id_string[id_len / 2..] {
+        true
+    } else {
+        false
+    }
+}
+
+pub fn sum_invalid_ids(idranges: &IDRanges) -> u64 {
+    idranges
+        .ranges
+        .iter()
+        .fold(0, |mut sum_invalid_ids, range| {
+            for id in range.start..=range.end {
+                if is_invalid_id(id) {
+                    sum_invalid_ids += id
+                }
+            }
+            sum_invalid_ids
+        })
+}
+
 #[cfg(test)]
 mod tests {
+    use super::is_invalid_id;
+
     use super::IDRange;
     use super::IDRanges;
     use super::parse_idrange;
+    use super::sum_invalid_ids;
+
+    use rstest::rstest;
 
     #[test]
     fn test_parse_idrange() {
@@ -63,6 +104,20 @@ mod tests {
         };
 
         assert_eq!(parse_idrange(input).unwrap().1, expected);
+    }
+
+    #[rstest]
+    #[case(11, true)]
+    #[case(22, true)]
+    #[case(99, true)]
+    #[case(101, false)]
+    #[case(1188511885, true)]
+    #[case(222222, true)]
+    #[case(1698522, false)]
+    #[case(446446, true)]
+    #[case(38593859, true)]
+    fn test_is_invalid_id(#[case] input: u64, #[case] expected: bool) {
+        assert_eq!(is_invalid_id(input), expected);
     }
 
     #[test]
@@ -83,8 +138,6 @@ mod tests {
             }
         );
 
-        todo!("Ensure the whole range or part of it is splittable in n (2) and
-        check the possible number starting with 1-9 whether they can make up
-        numbers in the series if repeated");
+        assert_eq!(sum_invalid_ids(&idranges), 1227775554);
     }
 }

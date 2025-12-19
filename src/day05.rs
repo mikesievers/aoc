ba72c70fc734e6ce659b93f2da5e39e93b55e7d2
Michael Sievers <michael@hamburg.de>
2025-12-19 09:02:14 +0100

    Load day05 sample

---
+++ src/day05.rs
@@ -0,0 +1,62 @@
+//! Advent of Code 2025, Day 05
+
+use nom::{
+    IResult, Parser,
+    character::complete::{char, line_ending, u64},
+    multi::{many1, separated_list1},
+    sequence::separated_pair,
+};
+use std::fs::read_to_string;
+
+pub struct Range {
+    start: u64,
+    end: u64,
+}
+
+pub struct Database {
+    ranges: Vec<Range>,
+    inventory: Vec<u64>,
+}
+
+impl Database {
+    pub fn from_file(fname: &str) -> Self {
+        let data = read_to_string(fname).unwrap();
+
+        let (_, database) = parse_database(data.as_str()).unwrap();
+
+        database
+    }
+}
+
+// Parsers
+fn parse_database(input: &str) -> IResult<&str, Database> {
+    let (rest, (ranges_raw, inventory)) = separated_pair(
+        separated_list1(line_ending, separated_pair(u64, char('-'), u64)),
+        many1(line_ending),
+        separated_list1(line_ending, u64),
+    )
+    .parse(input)?;
+    let ranges = ranges_raw
+        .into_iter()
+        .map(|x| Range {
+            start: x.0,
+            end: x.1,
+        })
+        .collect();
+    Ok((rest, Database { ranges, inventory }))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Database;
+
+    #[test]
+    fn test_x() {
+        let database = Database::from_file("input/day05_sample.txt");
+
+        assert_eq!(database.ranges[0].start, 3);
+        assert_eq!(database.ranges[3].end, 18);
+        assert_eq!(database.inventory[0], 1);
+        assert_eq!(database.inventory[5], 32);
+    }
+}

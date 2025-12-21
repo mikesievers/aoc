16222c3fc545352dd5f9f71fd715967380c5ab57
Michael Sievers <michael@hamburg.de>
2025-12-21 18:44:25 +0000

    Solved day07 part 1

--- src/day07.rs
+++ src/day07.rs
@@ -1,9 +1,15 @@
 //! Advent of Code 2025 Day 07
+//! ```
+//! use aoc::day07::Manifold;
+//! let manifold = Manifold::from_file("input/day07_input.txt");
+//! assert_eq!(manifold.n_splits(), 1550);
+//! ```
 
 use glam::IVec2;
 use std::{collections::HashSet, fs::read_to_string};
 
 pub struct Manifold {
+    height: usize,
     beam_pos: IVec2,
     splitters: HashSet<IVec2>,
 }
@@ -11,6 +17,8 @@ pub struct Manifold {
 impl Manifold {
     pub fn from_file(fname: &str) -> Self {
         let data = read_to_string(fname).unwrap();
+
+        let height = data.lines().filter(|x| !x.is_empty()).count();
 
         let beam_pos = *data
             .lines()
@@ -43,9 +51,42 @@ impl Manifold {
             .collect();
 
         Manifold {
+            height,
             beam_pos,
             splitters,
         }
+    }
+
+    pub fn n_splits(&self) -> usize {
+        // propagate or split the beams into the next row until the last row
+        // for row in self.
+        let mut beams: HashSet<IVec2> = HashSet::new();
+        beams.insert(self.beam_pos);
+
+        let mut n_splits = 0;
+
+        for _ in 0..self.height {
+            let mut new_beams: HashSet<IVec2> = HashSet::new();
+            for beam in &beams {
+                // The candidate coordinates are straight down
+                let x = beam.x;
+                let y = beam.y + 1;
+
+                match self.splitters.contains(&IVec2 { x, y }) {
+                    false => {
+                        new_beams.insert(IVec2 { x, y });
+                    }
+                    true => {
+                        n_splits += 1;
+                        new_beams.insert(IVec2 { x: x - 1, y });
+                        new_beams.insert(IVec2 { x: x + 1, y });
+                    }
+                }
+            }
+            beams = new_beams;
+        }
+
+        n_splits
     }
 }
 
@@ -58,10 +99,14 @@ mod tests {
     fn test_manifold() {
         let manifold = Manifold::from_file("input/day07_sample.txt");
 
+        assert_eq!(manifold.height, 16);
+
         assert_eq!(manifold.beam_pos, IVec2 { x: 7, y: 1 });
         assert!(manifold.splitters.contains(&IVec2 { x: 7, y: 2 }));
         assert!(manifold.splitters.contains(&IVec2 { x: 1, y: 14 }));
         assert!(!manifold.splitters.contains(&IVec2 { x: 2, y: 14 }));
         assert!(manifold.splitters.contains(&IVec2 { x: 3, y: 14 }));
+
+        assert_eq!(manifold.n_splits(), 21);
     }
 }

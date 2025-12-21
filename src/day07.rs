eb1b31848ff673f1f727b6495583bb9aebf42ab6
Michael Sievers <michael@hamburg.de>
2025-12-21 16:39:41 +0000

    Start day 07

---
+++ src/day07.rs
@@ -0,0 +1,21 @@
+//! Advent of Code 2025 Day 07
+
+pub struct Manifold {}
+
+impl Manifold {
+    pub fn from_file(fname: &str) -> Self {
+        Manifold {}
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Manifold;
+
+    #[test]
+    fn test_manifold() {
+        let manifold = Manifold::from_file("input/day07_sample.txt");
+
+        assert_eq!(false, true);
+    }
+}

603f2b4faba301b2b3e2aca072cd2947c74d4d13
Michael Sievers <michael@hamburg.de>
2025-12-30 16:41:49 +0100

    Do not use const generics

--- src/lights_out.rs
+++ src/lights_out.rs
@@ -17,56 +17,77 @@
 //! The transformation can be written as
 //!  A*x = p-b
 //! Rather than trying to determine an inverse of the matrix,
-//! guassion eliminationi s performed.
+//! guassion elimination is performed.
 //!
 use std::ops::{Add, Div, Sub};
 
-type LightIndex = usize;
-type Button = Vec<LightIndex>;
+// Light cycle delta
+type LightStateDelta = i32;
+// Which cycle changes does a button press cause
+type Button = Vec<LightStateDelta>;
 
 // Define a LightState that supports modular arithmetic
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
-struct LightState<const N: i32>(i32);
-
-// Allow initialization from i32
-impl<const N: i32> From<i32> for LightState<N> {
-    fn from(value: i32) -> Self {
-        LightState(value.rem_euclid(N))
-    }
+pub struct LightState {
+    v: i32,
+    dim: i32,
 }
 
 // implement modular airithmmetic
-impl<const N: i32> Add for LightState<N> {
+impl Add<LightState> for LightState {
     type Output = Self;
 
     fn add(self, rhs: Self) -> Self {
-        LightState((self.0 + rhs.0).rem_euclid(N))
+        if self.dim != rhs.dim {
+            panic!("Can't add LightStates with different dimension")
+        }
+        LightState {
+            v: (self.v + rhs.v).rem_euclid(self.dim),
+            dim: self.dim,
+        }
     }
 }
 
-impl<const N: i32> Sub for LightState<N> {
-    type Output = Self;
+impl Add<LightStateDelta> for LightState {
+    type Output = LightState;
 
-    fn sub(self, rhs: Self) -> Self {
-        LightState((self.0 - rhs.0).rem_euclid(N))
+    fn add(self, rhs: LightStateDelta) -> Self {
+        LightState {
+            v: (self.v + rhs).rem_euclid(self.dim),
+            dim: self.dim,
+        }
+    }
+}
+
+impl Sub for LightState {
+    type Output = LightStateDelta;
+
+    fn sub(self, rhs: Self) -> LightStateDelta {
+        if self.dim != rhs.dim {
+            panic!("Can't subtract LightStates with different dimension")
+        }
+        (self.v - rhs.v).rem_euclid(self.dim)
     }
 }
 // Division is multiplication by multiplicative inverse:
-impl<const N: i32> Div for LightState<N> {
+impl Div<LightStateDelta> for LightState {
     type Output = Self;
 
-    fn div(self, rhs: Self) -> Self {
+    fn div(self, rhs: LightStateDelta) -> Self {
         // Determine multiplicative inverse
         let mut mult_inv: i32 = -1;
-        for i in 0..N {
-            if (rhs.0 * i).rem_euclid(N) == 1 {
+        for i in 0..self.dim {
+            if (rhs * i).rem_euclid(self.dim) == 1 {
                 mult_inv = i;
             }
         }
         if mult_inv == -1 {
             panic!("Multiplicative inverse in base could not be found.");
         } else {
-            LightState((self.0 * mult_inv).rem_euclid(N))
+            LightState {
+                v: (self.v * mult_inv).rem_euclid(self.dim),
+                dim: self.dim,
+            }
         }
     }
 }
@@ -74,53 +95,51 @@ impl<const N: i32> Div for LightState<N> {
 #[cfg(test)]
 mod test_light_state {
     use super::LightState;
+    use super::LightStateDelta;
 
     #[test]
     fn test_addition() {
-        let ls1: LightState<3> = 1.into();
-        let ls2: LightState<3> = 2.into();
-        let ls3: LightState<3> = 5.into();
+        let ls1 = LightState { v: 1, dim: 3 };
+        let ls2 = LightState { v: 2, dim: 3 };
+        let ls3 = LightState { v: 5, dim: 3 };
 
         assert_eq!(ls1 + ls1, ls2); // 1+1 == 2
-        assert_eq!(ls3 - ls1, ls1); // 2 - 1 == 1
+        assert_eq!(ls3 - ls1, 1); // 2 - 1 == 1
     }
 
     #[test]
     fn test_division() {
-        let ls1: LightState<7> = 6.into();
-        let ls2: LightState<7> = 3.into();
-        assert_eq!(ls1 / ls2, 2.into());
+        let ls1 = LightState { v: 6, dim: 7 };
+        let ls2: LightStateDelta = 3;
+        assert_eq!(ls1 / ls2, LightState { v: 2, dim: 7 });
     }
 
     #[test]
     #[should_panic]
     fn test_division_panic() {
         // 2 in mod 10 does not have a multiplicative inverse
-        let ls1: LightState<10> = 5.into();
-        let ls2: LightState<10> = 2.into();
+        let ls1: LightState = LightState { v: 5, dim: 10 };
+        let ls2: LightStateDelta = 2;
 
-        let this_should_panic = ls1 / ls2;
+        let _this_should_panic = ls1 / ls2;
     }
 }
 
-pub const DIM: i32 = 3;
+pub const DIM: i32 = 2;
 
 pub struct LightsOut {
-    dimension: usize,
     buttons: Vec<Button>,
-    initial_state: Vec<LightState<DIM>>,
-    target_state: Vec<LightState<DIM>>,
+    initial_state: Vec<LightState>,
+    target_state: Vec<LightState>,
 }
 
 impl LightsOut {
     pub fn new(
-        dimension: usize,
         buttons: Vec<Button>,
-        initial_state: Vec<LightState<DIM>>,
-        target_state: Vec<LightState<DIM>>,
+        initial_state: Vec<LightState>,
+        target_state: Vec<LightState>,
     ) -> LightsOut {
         LightsOut {
-            dimension,
             buttons,
             initial_state,
             target_state,
@@ -132,23 +151,72 @@ impl LightsOut {
         //! The output is a Vec of button presses.
         //! [1,2] means press button 0 once and button 1 twice.
         //!
+
+        // Set up a Matrix to represent the lights out problem
+        // A*x = p-b
+        Self::setup_matrix(&self.buttons, &self.initial_state, &self.target_state);
+
+        // TODO: While the Matrix is not in row echelon form,
+        // perform gaussian elimination
         Some(vec![0, 1])
+    }
+
+    pub fn setup_matrix(
+        buttons: &[Button],
+        initial_state: &[LightState],
+        target_state: &[LightState],
+    ) -> Vec<Vec<LightStateDelta>> {
+        // From buttons and number of lights, determine the initial state of the matrix
+        // The final column will be the right side of the equation A*x = p-b
+        // The COLUMNs of the matrix correspond to the buttons and their wiring (i.e. lights
+        // affected)
+        // The ROWs correspond to the lights
+        let mut matrix: Vec<Vec<LightStateDelta>> = Vec::with_capacity(target_state.len());
+        for light_idx in 0..target_state.len() {
+            // Create a row by the wirings of the buttons and the expected state minus the initial
+            // state
+            let mut row: Vec<LightStateDelta> = Vec::with_capacity(buttons.len() + 1);
+            for button in buttons {
+                row.push(button[light_idx]);
+            }
+            // Add the right side of the equation system as final column
+            row.push(target_state[light_idx] - initial_state[light_idx]);
+
+            matrix.push(row);
+        }
+
+        matrix
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::{Button, DIM, LightState, LightsOut};
+    use super::{Button, LightState, LightsOut};
+
+    #[test]
+    fn test_setup_matrix() {
+        let initial_state: Vec<LightState> =
+            vec![LightState { v: 0, dim: 2 }, LightState { v: 1, dim: 2 }]; // Light 0 is off, 1 is on
+        let target_state: Vec<LightState> =
+            vec![LightState { v: 0, dim: 2 }, LightState { v: 0, dim: 2 }]; // All lights should be out
+        // Button 0 switches light 0,
+        // button 1 switches lights 0 and 1
+        let buttons: Vec<Button> = vec![vec![1, 0], vec![1, 1]];
+        let matrix = LightsOut::setup_matrix(&buttons, &initial_state, &target_state);
+
+        println!("Matrix: {:?}", matrix);
+
+        assert_eq!(matrix, vec![vec![1, 1, 0], vec![0, 1, 1]]);
+    }
 
     #[test]
     fn test_lo() {
-        let dimension = 2;
-        let initial_state: Vec<LightState<DIM>> = vec![0.into(), 1.into()]; // Light 0 is off, 1 is on
-        let target_state: Vec<LightState<DIM>> = vec![0.into(), 0.into()]; // All lights should be out
+        let initial_state = vec![LightState { v: 0, dim: 2 }, LightState { v: 1, dim: 2 }]; // Light 0 is off, 1 is on
+        let target_state = vec![LightState { v: 0, dim: 2 }, LightState { v: 0, dim: 2 }]; // All lights should be out
         // Button 0 switches light 0,
         // button 1 switches lights 0 and 1
-        let buttons: Vec<Button> = vec![vec![0], vec![0, 1]];
-        let lo = LightsOut::new(dimension, buttons, initial_state, target_state);
+        let buttons: Vec<Button> = vec![vec![1, 0], vec![1, 1]];
+        let lo = LightsOut::new(buttons, initial_state, target_state);
 
         // Both buttons have to be pressed once:
         assert_eq!(lo.solution(), Some(vec![1, 1]));

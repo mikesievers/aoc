8b89443361db09b6c774d753ea98093e03993f2f
Michael Sievers <michael@hamburg.de>
2026-03-07 22:06:03 +0100

    Finish day11 part2

--- src/day11.rs
+++ src/day11.rs
@@ -18,6 +18,7 @@ struct Device {
 
 pub struct Rack {
     devices: HashMap<String, Device>,
+    path_cache: HashMap<String, u64>,
 }
 
 impl Rack {
@@ -28,7 +29,10 @@ impl Rack {
         for device in device_vec {
             devices.insert(device.id.clone(), device);
         }
-        Ok(Rack { devices })
+        Ok(Rack {
+            devices,
+            path_cache: HashMap::new(),
+        })
     }
 }
 
@@ -70,6 +74,64 @@ impl Rack {
     }
 }
 
+impl Rack {
+    pub fn nr_problematic_paths_to_out(
+        &mut self,
+        node: &str,
+        dac_seen: bool,
+        fft_seen: bool,
+    ) -> u64 {
+        // Count the number of different paths from given string to "out"
+
+        // Check against the cache first
+        if let Some(&known_paths) = self
+            .path_cache
+            .get(&(format!("{}{}{}", node.to_string(), dac_seen, fft_seen)))
+        {
+            return known_paths;
+        }
+
+        // If the exit node is found, count as 1
+        // IF both dac and fft have been seen along the way
+        if node == "out" {
+            if dac_seen && fft_seen {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+        // If this node is dac or fft, remember that they have been seen
+        let mut out_dac_seen = dac_seen;
+        let mut out_fft_seen = fft_seen;
+
+        if node == "dac" {
+            out_dac_seen = true
+        }
+        if node == "fft" {
+            out_fft_seen = true
+        }
+
+        // recursively find the paths from the nodes connected to the current one
+        self.devices[node]
+            .conns
+            .clone()
+            .iter()
+            .map(|node| {
+                let val = self.nr_problematic_paths_to_out(
+                    node.clone().as_str(),
+                    out_dac_seen,
+                    out_fft_seen,
+                );
+                self.path_cache.insert(
+                    format!("{}{}{}", node.clone(), out_dac_seen, out_fft_seen),
+                    val,
+                );
+                val
+            })
+            .sum()
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -94,6 +156,23 @@ mod tests {
         let rack = Rack::from_file("input/day11_input.txt")?;
 
         assert_eq!(rack.nr_paths_to_out("you"), 670);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_rack_pt2() -> Result<()> {
+        let mut rack = Rack::from_file("input/day11_sample_2.txt")?;
+
+        assert_eq!(rack.nr_problematic_paths_to_out("svr", false, false), 2);
+
+        Ok(())
+    }
+    #[test]
+    fn test_day11_2() -> Result<()> {
+        let mut rack = Rack::from_file("input/day11_input.txt")?;
+
+        assert_eq!(rack.nr_problematic_paths_to_out("svr", false, false), 332052564714990);
 
         Ok(())
     }
--- src/lib.rs
+++ src/lib.rs
@@ -13,5 +13,5 @@ pub mod day07;
 pub mod day08;
 pub mod day09;
 pub mod day10;
-pub mod lights_out;
 pub mod day11;
+pub mod lights_out;
